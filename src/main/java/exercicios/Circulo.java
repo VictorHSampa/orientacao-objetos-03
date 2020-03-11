63ecf9f3f3dab70eb84860ff268c37c125ad0de3
VictorHSampa <victorhugosampa@gmail.com>
2020-03-11 20:58:50 -0300

    Criacao da classe Circulo e CirculoTeste

---
+++ src/main/java/exercicios/Circulo.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+public class Circulo {
+    private Double raio;
+
+    public Circulo(Double raio){
+        setRaio(raio);
+    }
+
+    public void setRaio(Double raio) {
+        if(raio <= 0){
+            throw new IllegalArgumentException("O raio deve ser positivo");
+        }
+        this.raio = raio;
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public Double calcArea(){
+        return (raio * raio) * 3.0;
+    }
+    public Double calcPerimetro(){
+        return (2.0*raio)*3.0;
+    }
+}
